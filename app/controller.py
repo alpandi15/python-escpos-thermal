2cf6f9a7b872e456ba6e7d4ee433a174a6316d6f
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2020-09-30 15:07:24 +0700

    Update response

--- app/controller.py
+++ app/controller.py
@@ -15,31 +15,49 @@ def index():
 @app.route('/print', methods = ['POST'])
 def print():
     if request.method == "POST":
+        # get json data from request
         requestBody = request.get_json()
         fileString = requestBody.get('image', False)
+        ipPrintert = requestBody.get('network', False)
 
+        # check required body request
         if fileString == '' or fileString == False:
             return make_response(jsonify(
                 message='Required image',
                 success=False
             ), 422)
-
-        imgdata = service.base64ToImage(fileString)
-
-        if imgdata == '' or imgdata == False:
+        if ipPrintert == '' or ipPrintert == False:
             return make_response(jsonify(
-                message='Convert base64 to image error',
+                message='Required network',
                 success=False
             ), 422)
 
-        printer = Network(host='192.168.1.216')
-        printer.image(imgdata)
-        printer.cut()
+        try:
+            # service convert base64 to image
+            imgdata = service.base64ToImage(fileString)
 
-        return make_response(
-            jsonify(
-                success=True,
-                printer=fileString
-            ),
-            200
-        )
+            if imgdata == '' or imgdata == False:
+                return make_response(jsonify(
+                    message='Convert base64 to image error',
+                    success=False
+                ), 422)
+
+            # print to thermal escpos
+            printer = Network(host=ipPrintert, port=9100, timeout=60)
+            printer.image(imgdata)
+            printer.cut()
+
+            # printer.cashdraw('pin')
+
+            return make_response(
+                jsonify(
+                    success=True,
+                    message='Success print to thermal '+ipPrintert
+                ),
+                200
+            )
+        except Exception as e:
+            return make_response(jsonify(
+                message=e,
+                success=False
+            ), 500)
